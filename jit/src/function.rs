f6c6a3cb974edbd6a2e8bb6c8141465729b5caed
Ubugeeei <ubuge1122@gamil.com>
2022-12-29 20:45:01 +0900

    feat: compile function

---
+++ jit/src/function.rs
@@ -0,0 +1,76 @@
+use ast::Prototype;
+use inkwell::{types::BasicMetadataTypeEnum, values::FunctionValue};
+
+use crate::Compiler;
+
+impl<'a, 'ctx> Compiler<'a, 'ctx> {
+    pub(super) fn compile_prototype(
+        &self,
+        proto: &Prototype,
+    ) -> Result<FunctionValue<'ctx>, &'static str> {
+        let ret_type = self.context.f64_type();
+        let args_types = std::iter::repeat(ret_type)
+            .take(proto.args.len())
+            .map(|f| f.into())
+            .collect::<Vec<BasicMetadataTypeEnum>>();
+        let args_types = args_types.as_slice();
+
+        let fn_type = self.context.f64_type().fn_type(args_types, false);
+        let fn_val = self.module.add_function(proto.name.as_str(), fn_type, None);
+
+        // set arguments names
+        for (i, arg) in fn_val.get_param_iter().enumerate() {
+            arg.into_float_value().set_name(proto.args[i].as_str());
+        }
+
+        // finally return built prototype
+        Ok(fn_val)
+    }
+
+    pub(super) fn compile_fn(&mut self) -> Result<FunctionValue<'ctx>, &'static str> {
+        let proto = &self.function.prototype;
+        let function = self.compile_prototype(proto)?;
+
+        // got external function, returning only compiled prototype
+        if self.function.body.is_none() {
+            return Ok(function);
+        }
+
+        let entry = self.context.append_basic_block(function, "entry");
+
+        self.builder.position_at_end(entry);
+
+        // update fn field
+        self.fn_value_opt = Some(function);
+
+        // build variables map
+        self.variables.reserve(proto.args.len());
+
+        for (i, arg) in function.get_param_iter().enumerate() {
+            let arg_name = proto.args[i].as_str();
+            let alloca = self.create_entry_block_alloca(arg_name);
+
+            self.builder.build_store(alloca, arg);
+
+            self.variables.insert(proto.args[i].clone(), alloca);
+        }
+
+        // compile body
+        let body = self.compile_expr(self.function.body.as_ref().unwrap())?;
+
+        self.builder.build_return(Some(&body));
+
+        // return the whole thing after verification and optimization
+        if function.verify(true) {
+            self.fpm.run_on(&function);
+
+            Ok(function)
+        } else {
+            unsafe {
+                function.delete();
+            }
+
+            Err("Invalid generated function.")
+        }
+    }
+}
