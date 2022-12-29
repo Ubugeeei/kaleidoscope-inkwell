4d3f1bc26654de3f97c1f99babfee36683c2b818
Ubugeeei <ubuge1122@gamil.com>
2022-12-29 21:27:47 +0900

    parser example

--- crates/parse/examples/parse.rs
+++ crates/parse/examples/parse.rs
@@ -1,7 +1,12 @@
+use std::io::Write;
+
 use parse::Parser;
 
 fn main() {
     loop {
+        print!("> ");
+        let _ = std::io::stdout().flush();
+
         let mut input = String::new();
         std::io::stdin().read_line(&mut input).unwrap();
         let mut parser = Parser::new(input);
