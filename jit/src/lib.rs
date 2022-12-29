7ba5f13a9595b1bb06808ccd4f6bfbdb78c60272
Ubugeeei <ubuge1122@gamil.com>
2022-12-29 20:45:31 +0900

    feat: jit compiler

--- jit/src/lib.rs
+++ jit/src/lib.rs
@@ -1,14 +1,67 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use std::collections::HashMap;
+
+use ast::Function;
+use inkwell::builder::Builder;
+use inkwell::context::Context;
+use inkwell::module::Module;
+use inkwell::passes::PassManager;
+use inkwell::values::{FunctionValue, PointerValue};
+
+mod expression;
+mod function;
+
+pub struct Compiler<'a, 'ctx> {
+    pub context: &'ctx Context,
+    pub builder: &'a Builder<'ctx>,
+    pub fpm: &'a PassManager<FunctionValue<'ctx>>,
+    pub module: &'a Module<'ctx>,
+    pub function: &'a Function,
+
+    variables: HashMap<String, PointerValue<'ctx>>,
+    fn_value_opt: Option<FunctionValue<'ctx>>,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+impl<'a, 'ctx> Compiler<'a, 'ctx> {
+    #[inline]
+    fn get_function(&self, name: &str) -> Option<FunctionValue<'ctx>> {
+        self.module.get_function(name)
+    }
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    #[inline]
+    pub fn compile(
+        context: &'ctx Context,
+        builder: &'a Builder<'ctx>,
+        pass_manager: &'a PassManager<FunctionValue<'ctx>>,
+        module: &'a Module<'ctx>,
+        function: &Function,
+    ) -> Result<FunctionValue<'ctx>, &'static str> {
+        let mut compiler = Compiler {
+            context,
+            builder,
+            fpm: pass_manager,
+            module,
+            function,
+            fn_value_opt: None,
+            variables: HashMap::new(),
+        };
+
+        compiler.compile_fn()
+    }
+
+    fn create_entry_block_alloca(&self, name: &str) -> PointerValue<'ctx> {
+        let builder = self.context.create_builder();
+
+        let entry = self.fn_value().get_first_basic_block().unwrap();
+
+        match entry.get_first_instruction() {
+            Some(first_instr) => builder.position_before(&first_instr),
+            None => builder.position_at_end(entry),
+        }
+
+        builder.build_alloca(self.context.f64_type(), name)
+    }
+
+    fn fn_value(&self) -> FunctionValue<'ctx> {
+        self.fn_value_opt.unwrap()
     }
 }
