df88c9c1642479dbb347d842fc78d7ace2d5d9cd
Ubugeeei <ubuge1122@gamil.com>
2022-12-30 10:54:48 +0900

    tets: parse number

--- parse/src/expression/number.rs
+++ parse/src/expression/number.rs
@@ -14,3 +14,29 @@ impl Parser {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_parse_number() {
+        let mut parser = Parser::new("42".to_string());
+        let result = parser.parse_number_expr();
+        assert_eq!(result, Ok(Expression::Number(42.0)));
+    }
+
+    #[test]
+    fn test_parse_number_float() {
+        let mut parser = Parser::new("42.2".to_string());
+        let result = parser.parse_number_expr();
+        assert_eq!(result, Ok(Expression::Number(42.2)));
+    }
+
+    #[test]
+    fn test_parse_number_fail() {
+        let mut parser = Parser::new("42.2.1".to_string());
+        let result = parser.parse_number_expr();
+        assert_eq!(result, Err("Expected number literal."));
+    }
+}
